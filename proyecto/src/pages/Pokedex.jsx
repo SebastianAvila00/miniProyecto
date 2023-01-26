546a393abf4ee7aff714d8108e3b4cfe113916a7
SebastianAvila00 <sebastian.avila@arbusta.net>
2023-01-26 14:20:58 -0300

    ver mas

--- proyecto/src/pages/Pokedex.jsx
+++ proyecto/src/pages/Pokedex.jsx
@@ -24,11 +24,18 @@ function Pokedex() {
     const [search, setSearch] = useState("");
     const [changePokemon, setChangePokemon] = useState();
 
+
+    const [visible, setVisible] = useState(12);
+      const showMoreItems = () => {
+        setVisible((prevValue) => prevValue + 12);
+    };
+
+
     //LLAMADO A POKEAPI
     const peticionGet = async () => {
         await axios.get("https://pokeapi.co/api/v2/pokemon/")
             .then(response => {
-                setChangePokemon(response.data.results.slice(0, 12));
+                setChangePokemon(response.data.results);
                 console.log(response.data.results);
             }).catch(error => {
                 console.log(error);
@@ -57,15 +64,6 @@ function Pokedex() {
         )
     }
 
-
-    // const PokeRandom = (min, max) => {
-    //     return Math.floor((Math.random() * (max - min + 1)))
-    // }
-    // for (let x = 0; x < 100; x++) {
-    //     console.log(PokeRandom(0, 12));
-    // }
-
-    // console.log(PokeRandom);
 
     return (
         <div className="h-screen">
@@ -96,7 +94,7 @@ function Pokedex() {
                             <div className=" lg:h-[194px] lg:flex-row md:h-[300px] md:w-full flex flex-col justify-center items-center bg-[#313131] md:flex lg:justify-center md:items-center xl:w-full">
 
                                 <div className="md:w-full lg:w-[400px]">
-                                    <p className="lg:text-left text-center mt-2 text-[#fff] mb-3 text-[27px]">Nombre o número</p>
+                                    <p className="lg:text-left lg:ml-1 text-center mt-2 text-[#fff] mb-3 text-[27px]">Nombre o número</p>
 
                                     <div className="justify-center md:items-center md:justify-center flex lg:flex">
 
@@ -114,13 +112,13 @@ function Pokedex() {
 
                                     </div>
 
-                                    <p className="text-center lg:text-left m-auto md:text-center md:w-[400px] lg:w-[450px] text-[12px] md:text-[16px] text-white font-semibold">¡Usa la búsqueda avanzada para encontrar  Pokémon por su tipo, debilidad,habilidad y demas datos!</p>
-
+                                    <p className="text-center lg:ml-1 lg:text-left m-auto md:text-center md:w-[400px] lg:w-[450px] text-[12px] md:text-[16px] text-white font-semibold">¡Usa la búsqueda avanzada para encontrar  Pokémon por su tipo, debilidad,habilidad y demas datos!</p>
+                
                                 </div>
 
                                 <div>
 
-                                    <div className="lg:ml-10 w-full md:w-screen md:mt-5 mb-3 mt-3 h-[90px] lg:w-[430px] lg:mb-[54px] rounded-md bg-[#4dad5b] flex items-center">
+                                    <div className="lg:ml-[49px] w-full md:w-screen md:mt-5 mb-3 mt-3 h-[90px] lg:h-[88px] lg:w-[430px] lg:mb-[54px] rounded-[5px] bg-[#4dad5b] flex items-end">
 
                                         <p className="lg:font-medium lg:text-left md:text-center md:text-[25px] text-[15px] p-3 md:p-5 text-white lg:text-[20px] ">Busca un Pokémon por su nombre o <br className="md:hidden" /> usando su número de la Pokédex Nacional.</p>
                                     </div>
@@ -170,7 +168,7 @@ function Pokedex() {
 
                                     <tbody className="flex flex-wrap justify-center">
 
-                                        {!results ? "cargando..." : results.map((pokemon, index) => {
+                                        {!results ? "cargando..." : results.slice(0, visible).map((pokemon, index) => {
                                             return (
 
                                                 // PARA MATCHEAR LA IMG CON EL NOMBRE DEL INPUT LE SUMO UN +1 AL URL DE LAS IMAGENES PARA QUE MIENTRAS TRAIGA LOS ID TAMBIEN TRAIGA LA IMAGEN DEL RESPECTIVO NUMERO
@@ -196,7 +194,7 @@ function Pokedex() {
 
                             <div className="flex justify-center mt-[20px] mb-[30px]">
 
-                                <div className="bg-[#30a7d7] rounded-md h-[40px] w-[200px] flex items-center justify-center cursor-pointer hover:bg-[#178bb8]">
+                                <div onClick={showMoreItems} className="bg-[#30a7d7] rounded-md h-[40px] w-[200px] flex items-center justify-center cursor-pointer hover:bg-[#178bb8]">
 
                                     <p className="text-[white] font-semibold">Cargar más Pokémon</p>
                                 </div>
